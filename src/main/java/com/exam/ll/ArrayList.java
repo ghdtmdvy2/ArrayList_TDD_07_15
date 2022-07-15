39da77a9bcf5c6c68f310bcc31fc6cfcd71fd774
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-15 10:46:07 +0900

    add를_한번_할_때_마다_size가_증가

--- src/main/java/com/exam/ll/ArrayList.java
+++ src/main/java/com/exam/ll/ArrayList.java
@@ -1,11 +1,26 @@
 package com.exam.ll;
 
 public class ArrayList {
+    int datas[];
+    int lastIndex;
+    ArrayList(){
+        datas = new int[2];
+        lastIndex = -1;
+    }
     public int size() {
-        return 0;
+        return lastIndex+1;
     }
 
-    public void add(int i) {
+    public void add(int data) {
+        if(lastIndex + 1 >= datas.length){
+            int newArr[] = new int[datas.length * 2];
+            for(int i = 0; i<size(); i++){
+                newArr[i] = datas[i];
+            }
+            datas = newArr;
+        }
+        lastIndex++;
+        datas[lastIndex] = data;
     }
 
     public int get(int i) {
