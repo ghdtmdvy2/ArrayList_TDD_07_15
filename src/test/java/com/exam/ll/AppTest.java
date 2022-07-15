b82fdf2179938bc93957be3b490939c13b868cfb
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-15 10:35:38 +0900

    ArrayList_클래스가_존재한다 TDD

---
+++ src/test/java/com/exam/ll/AppTest.java
@@ -0,0 +1,13 @@
+package com.exam.ll;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class AppTest {
+    @Test
+    void ArrayList_클래스가_존재한다() {
+        ArrayList al = new ArrayList();
+    }
+
+}
