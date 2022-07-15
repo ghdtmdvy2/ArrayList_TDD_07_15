daba2f92fc0c9ac616e35d0863050ce6b2a86075
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-15 10:39:24 +0900

    size TDD

---
+++ src/main/java/com/exam/ll/ArrayList.java
@@ -0,0 +1,7 @@
+package com.exam.ll;
+
+public class ArrayList {
+    public int size() {
+        return 0;
+    }
+}
--- src/test/java/com/exam/ll/AppTest.java
+++ src/test/java/com/exam/ll/AppTest.java
@@ -2,12 +2,18 @@ package com.exam.ll;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
     void ArrayList_클래스가_존재한다() {
         ArrayList al = new ArrayList();
+    }
+    @Test
+    void size() {
+        ArrayList al = new ArrayList();
+        assertEquals(0, al.size());
     }
 
 }
